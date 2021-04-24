1da5d2cc12f630fe7af49270410154f0539c748b
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-04-24 23:57:27 +0530

    Add basic keypress handling

---
+++ README.md
@@ -0,0 +1,18 @@
+# Blossom Algorithm
+
+Implementation and visualization of the Blossom algorithm.
+
+## Installation
+Make sure you're running python 3.6.x
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+```
+python main.py
+```
+
+Press `enter` to update graph.
--- main.py
+++ main.py
@@ -4,6 +4,19 @@ import networkx as nx
 from src import sample_graphs
 
 g = sample_graphs.diamond()
+g1 = sample_graphs.peterson()
+
+
+def press(event):
+    print('[DEBUG] pressed', event.key)
+    if event.key == 'enter':
+        fig.clear()
+        nx.draw(g1)
+        fig.canvas.draw()
+
+fig, ax = plt.subplots()
+
+fig.canvas.mpl_connect('key_press_event', press)
 
 nx.draw(g)
 plt.show()
