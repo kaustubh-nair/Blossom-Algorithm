8fb6d63db2ecf7b977bcfd1312c5274612c5eb0f
sarthak <sarthak.khoche@gmail.com>
2021-04-25 15:57:30 +0530

    added pseudocode

--- src/blossom.py
+++ src/blossom.py
@@ -3,32 +3,56 @@ import time
 
 from .helpers import update, update_colors
 
+def find_free_vertices(g):
+    free_vertices = []
+    return free_vertices
+
+def pick_random(free_vertices):
+    return random_vertex
+
+def BFS(g, random_vertex):
+    return next_free_vertex, vertices #all the vertices in between the random vertex and the final free vertex
+
+def augmenting_path(g, random_vertex, vertices, next_free_vertex):
+    return    
+
+def invert_augmenting_path(g, random_vertex, vertices, next_free_vertex):
+    return edges
+
+def update_graph(g, matching):
+    return g #To update the new matches vertices
+
 # Algorithm goes here
 def run(g, colors, animation_data):
+    matching = []
+    free_vertices = find_free_vertices(g)
 
-    # Make sure to update graph after every important step, which should be shown in animation.
-    g.remove_edge(1, 2)
-    update(animation_data, g, colors)
+    while(len(free_vertices) != 0):
 
-    g.remove_edge(3, 2)
-    update(animation_data, g, colors)
+        #start with displaying the graph g 
 
-    g.remove_edge(3, 1)
-    update(animation_data, g, colors)
+        free_vertices = find_free_vertices(g)
+        # color all free vertices
+        update(animation_data, g, colors)
 
-    g.remove_edge(4, 5)
-    update(animation_data, g, colors)
+        random_vertex = pick_random(free_vertices)
+        # highlight the chosen random vertex
+        update(animation_data, g, colors)
 
-    update_colors(colors, 3, 4, 'green')
-    update(animation_data, g, colors)
+        next_free_vertex, vertices = BFS(g, random_vertex)
+        # highlight the next free vertex
+        # this step can be further subdivided -- showing all animations till finding the next free vertex
+        # also add the blossom part in this
+        update(animation_data, g, colors)
 
-    update_colors(colors, 1, 4, 'red')
-    update(animation_data, g, colors)
+        augmenting_path(g, random_vertex, vertices, next_free_vertex)
+        # highlight the entire augmenting path
 
-    update_colors(colors, 2, 4, 'yellow')
-    update(animation_data, g, colors)
+        edges = invert_augmenting_path(g, random_vertex, vertices, next_free_vertex)
+        matching.append(edges)
+        # highlight the inverted augmenting path
 
-    update_colors(colors, 8, 9, 'blue')
-    update(animation_data, g, colors)
+        g = update_graph(g, matching)
+        free_vertices = find_free_vertices(g)
 
     return animation_data
